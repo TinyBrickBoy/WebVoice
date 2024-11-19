21adbfc48134baf938eaea241078ccb35b6e5084
booky10 <boooky10@gmail.com>
2024-11-19 09:49:36 +0100

    Fix websocket disconnection

--- src/scripts/socket.ts
+++ src/scripts/socket.ts
@@ -1,11 +1,13 @@
-export const buildSocket = (token: string, open: () => void) => {
+export const buildSocket = (token: string, onOpen: () => void, onClose: () => void) => {
     const socket = new WebSocket("wss://voice.tjcserver.net", [token])
-    socket.addEventListener("open", open)
+    socket.addEventListener("open", onOpen)
+    socket.addEventListener("close", onClose)
+    socket.addEventListener("error", (error) => {
+        console.error(error)
+        onClose();
+    })
     socket.addEventListener("message", (event) => {
         console.log("message", event)
-    })
-    socket.addEventListener("close", (event) => {
-        console.log("close", event)
     })
     return socket
 }
