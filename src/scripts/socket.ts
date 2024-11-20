6f694dfb79f902a14f814baeec0296e9106b5f90
booky10 <boooky10@gmail.com>
2024-11-20 14:16:46 +0100

    Rewrite to using sockette library
    This is wip

--- src/scripts/socket.ts
+++ src/scripts/socket.ts
@@ -1,13 +1,29 @@
-export const buildSocket = (token: string, onOpen: () => void, onClose: () => void) => {
-    const socket = new WebSocket("wss://voice.tjcserver.net", [token])
-    socket.addEventListener("open", onOpen)
-    socket.addEventListener("close", onClose)
-    socket.addEventListener("error", (error) => {
-        console.error(error)
-        onClose();
-    })
-    socket.addEventListener("message", (event) => {
-        console.log("message", event)
-    })
-    return socket
+import Sockette from "sockette"
+import EventEmitter from "node:events"
+
+const WEBSOCKET_SERVER = "wss://voice.tjcserver.net"
+
+export class VoiceSocket extends EventEmitter {
+
+    public socket: Sockette;
+
+    public constructor(player: string) {
+        super();
+        this.socket = new Sockette(WEBSOCKET_SERVER, {
+            timeout: 10e3,
+            maxAttempts: 10,
+            onopen: event => window.lllog("open", event),
+            onmessage: event => window.lllog("message", event),
+            onreconnect: event => window.lllog("reconnect", event),
+            onmaximum: event => window.lllog("maximum", event),
+            onclose: event => window.lllog("close", event),
+            onerror: event => window.lllog("error", event),
+            protocols: [ player ],
+        })
+    }
+
+    public open(token: string) {
+        this.socket.open();
+        this.socket.json({ key: "auth", packet: { token } })
+    }
 }
