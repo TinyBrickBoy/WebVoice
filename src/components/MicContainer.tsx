8d0f96ec241c29f064268a484e07bca90ad6675f
booky10 <boooky10@gmail.com>
2024-12-25 02:42:46 +0100

    Implement noise suppression using RNNoise and noise gates

--- src/components/MicContainer.tsx
+++ src/components/MicContainer.tsx
@@ -1,6 +1,10 @@
-import workletUrl from "../scripts/audio_visualizer.ts?worker&url";
+import noiseGateWorkletPath from "@sapphi-red/web-noise-suppressor/noiseGateWorklet.js?url";
+import rnnoiseWorkletPath from "@sapphi-red/web-noise-suppressor/rnnoiseWorklet.js?url";
+import rnnoiseWasmPath from "@sapphi-red/web-noise-suppressor/rnnoise.wasm?url";
+import rnnoiseWasmSimdPath from "@sapphi-red/web-noise-suppressor/rnnoise_simd.wasm?url";
 import type {FunctionComponent} from "preact";
-import {useEffect, useMemo, useRef} from "preact/hooks";
+import {useEffect, useRef} from "preact/hooks";
+import {loadRnnoise, NoiseGateWorkletNode, RnnoiseWorkletNode} from "@sapphi-red/web-noise-suppressor";
 
 export type VisualizerInitMessage = {
     canvas: HTMLCanvasElement;
@@ -11,6 +15,61 @@ export type VisualizerFrameMessage = {
 }
 export type VisualizerMessage = VisualizerInitMessage | VisualizerFrameMessage;
 
+const setupAudioContext = async (ctx: AudioContext, analyzers: AnalyserNode[]) => {
+    // load WASM
+    const rnnoiseWasmBinary = await loadRnnoise({
+        url: rnnoiseWasmPath,
+        simdUrl: rnnoiseWasmSimdPath,
+    });
+
+    // load worker modules
+    await ctx.audioWorklet.addModule(noiseGateWorkletPath);
+    await ctx.audioWorklet.addModule(rnnoiseWorkletPath);
+
+    // load microphone stream
+    const micStream = await navigator.mediaDevices!!.getUserMedia({audio: true});
+
+    // convert to mono
+    const mono = new MediaStreamAudioDestinationNode(ctx, {channelCount: 1});
+    ctx.createMediaStreamSource(micStream).connect(mono);
+    const monoNode = ctx.createMediaStreamSource(mono.stream)
+        .connect(analyzers.shift()!!);
+
+    // change volume
+    const gainedNode = monoNode
+        .connect(new GainNode(ctx, {gain: 0.1}));
+
+    // apply RNNoise
+    const noisedNode = gainedNode
+        .connect(new RnnoiseWorkletNode(ctx, {
+            wasmBinary: rnnoiseWasmBinary,
+            maxChannels: 1,
+        }))
+        .connect(analyzers.shift()!!);
+
+    // apply noise gate
+    const gatedNode = noisedNode
+        .connect(new NoiseGateWorkletNode(ctx, {
+            openThreshold: -50,
+            closeThreshold: -60,
+            holdMs: 150,
+            maxChannels: 1,
+        }))
+        .connect(new GainNode(ctx, {gain: 1}))
+        .connect(analyzers.shift()!!);
+
+    // apply "debug"
+    // gatedNode.connect(ctx.destination);
+};
+const setupAudioAnalyzer = (ctx: AudioContext) => {
+    const analyzer = ctx.createAnalyser();
+    analyzer.fftSize = 256;
+    analyzer.minDecibels = -90;
+    analyzer.maxDecibels = -30;
+    analyzer.smoothingTimeConstant = 0.5;
+    return analyzer;
+};
+
 const MicContainer: FunctionComponent<{}> = (props) => {
     const canvasRef = useRef<HTMLCanvasElement>();
 
@@ -19,33 +78,40 @@ const MicContainer: FunctionComponent<{}> = (props) => {
         const canvas = canvasRef.current;
 
         const audioCtx = new AudioContext();
-        const analyzer = audioCtx.createAnalyser();
-        analyzer.fftSize = 64;
-        analyzer.minDecibels = -90;
-        analyzer.maxDecibels = -30;
-        analyzer.smoothingTimeConstant = 0.85;
+        const preNoiseAnalyzer = setupAudioAnalyzer(audioCtx);
+        const postNoiseAnalyzer = setupAudioAnalyzer(audioCtx);
+        const postGateAnalyzer = setupAudioAnalyzer(audioCtx);
 
-        navigator.mediaDevices!!.getUserMedia({audio: true})
-            .then(stream => audioCtx.createMediaStreamSource(stream).connect(analyzer));
+        setupAudioContext(audioCtx, [preNoiseAnalyzer, postNoiseAnalyzer, postGateAnalyzer]);
 
         const draw = () => {
             requestAnimationFrame(draw);
 
-            const bufferLength = analyzer.frequencyBinCount;
-            const dataArray = new Uint8Array(bufferLength);
-            analyzer.getByteFrequencyData(dataArray);
+            const bufferLength = preNoiseAnalyzer.frequencyBinCount;
+            const preNoiseData = new Uint8Array(bufferLength);
+            preNoiseAnalyzer.getByteFrequencyData(preNoiseData);
+            const postNoiseData = new Uint8Array(bufferLength);
+            postNoiseAnalyzer.getByteFrequencyData(postNoiseData);
+            const postGateData = new Uint8Array(bufferLength);
+            postGateAnalyzer.getByteFrequencyData(postGateData);
 
             const ctx = canvas.getContext("2d");
             if (!ctx) return;
 
             ctx.fillStyle = "black";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.fillStyle = "white";
 
             const barWidth = canvas.width / bufferLength;
             for (let i = 0; i < bufferLength; ++i) {
-                const barHeight = dataArray[i];
-                ctx.fillRect(i * (barWidth - 1), canvas.height - barHeight, barWidth, barHeight);
+                const preNoiseHeight = preNoiseData[i];
+                const postNoiseHeight = postNoiseData[i];
+                const postGateHeight = postGateData[i];
+                ctx.fillStyle = "red";
+                ctx.fillRect(i * (barWidth - 1), canvas.height - preNoiseHeight, barWidth, preNoiseHeight - postNoiseHeight - postGateHeight);
+                ctx.fillStyle = "blue";
+                ctx.fillRect(i * (barWidth - 1), canvas.height - postNoiseHeight, barWidth, postNoiseHeight - postGateHeight);
+                ctx.fillStyle = "white";
+                ctx.fillRect(i * (barWidth - 1), canvas.height - postGateHeight, barWidth, postGateHeight);
             }
         };
         draw();
