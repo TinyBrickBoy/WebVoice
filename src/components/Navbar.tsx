dcb5f7c0c3463f9a423cd643172d554c290ba0b1
booky <booky@booky.dev>
2025-12-04 15:50:43 +0100

    Some voice category adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar.tsx";
 
 export const Navbar: FunctionComponent = () => {
     return <>
-        <div className={"flex flex-col min-w-110 p-4 gap-2 h-full w-1/2"}>
+        <div className={"flex flex-col p-4 gap-2 h-full w-1/2"}>
             <SearchBar/>
             <div className={"flex flex-col flex-1 gap-2 overflow-y-scroll grow pr-4"}>
                 <VoiceCategories/>
--- src/components/VolumeSlider.tsx
+++ src/components/VolumeSlider.tsx
@@ -1,5 +1,5 @@
 import {getVolume, setVolume, type VolumeType} from "../scripts/util/volumes.ts";
-import {useEffect, useRef} from "preact/hooks";
+import {useEffect, useRef, useState} from "preact/hooks";
 import type {FunctionComponent} from "preact";
 
 interface Props {
@@ -9,37 +9,24 @@ interface Props {
 }
 
 const VolumeSlider: FunctionComponent<Props> = (props) => {
-    const volume = getVolume(props.type, props.name);
-    const volumeRef = useRef<HTMLSpanElement>();
+    const [volumeSlider, setVolumeSlider] = useState<number>(getVolume(props.type, props.name));
 
     useEffect(() => {
         if (props.onUpdate) {
-            props.onUpdate(volume);
+            props.onUpdate(volumeSlider);
         }
-    }, [props.type, props.name, props.onUpdate]);
+        setVolume(props.type, props.name, volumeSlider, false);
+    }, [volumeSlider, props.onUpdate]);
 
     return (
-        <div className={"flex flex-col"}>
-            { /* @ts-ignore null apparently isn't undefined... thanks JS */}
-            <span className={"mt-2 mb-1 text-sm text-neutral-500"}>Volume</span>
-            <div className={"flex gap-5"}>
-                <input
-                    className={"w-full"}
-                    id={props.name} type={"range"} max={100} min={0} step={1} value={volume}
-                    onInput={event => {
-                        // instantly update displayed value, without saving
-                        const volume = +event.currentTarget.value;
-                        volumeRef.current!!.innerText = `${volume}`;
-                        setVolume(props.type, props.name, +volume, false);
-                        if (props.onUpdate) props.onUpdate(volume);
-                    }}
-                    onChange={event => {
-                        // only save once the mouse has been released
-                        setVolume(props.type, props.name, +event.currentTarget.value);
-                    }}
-                />
-                <label className={"min-w-10"} for={props.name}><span ref={volumeRef}>{volume}</span>%</label>
-            </div>
+        <div className={"flex flex-row gap-4 items-center"}>
+            <input
+                className={"grow rounded-lg bg-neutral-500 accent-neutral-500 cursor-pointer"} id={props.name} type={"range"}
+                max={100} min={0} step={1} value={volumeSlider}
+                onInput={event => setVolumeSlider(+event.currentTarget.value)} // don't save
+                onChange={() => setVolume(props.type, props.name, volumeSlider)} // save on release
+            />
+            <label className={"w-10"} for={props.name}>{volumeSlider}%</label>
         </div>
     );
 };
