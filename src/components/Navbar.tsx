75a00dc1e7cabf188bff1e2099393d491a068da8
SirReGa <jona.skrubel@gmail.com>
2025-12-04 16:26:58 +0100

    Style searchbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,11 @@ import SearchBar from "./SearchBar.tsx";
 
 export const Navbar: FunctionComponent = () => {
     return <>
-        <div className={"flex flex-col p-4 gap-2 h-full w-1/2"}>
+        <div className={"flex flex-col p-8 gap-2 h-full w-1/2"}>
+            <div className={"flex flex-col mb-6 w-full items-end"}>
+                <span className={"text-sm"}>Current group</span>
+                <span className={"text-xl"}>CURRENT GROUP NAME {/*TODO*/}</span>
+            </div>
             <SearchBar/>
             <div className={"flex flex-col flex-1 gap-2 overflow-y-scroll grow pr-4"}>
                 <VoiceCategories/>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,10 +2,10 @@ import type {FunctionComponent} from "preact";
 
 const SearchBar: FunctionComponent = () => {
     return <>
-        <label className={"flex flex-row gap-2"}>
-            Search
-            <input type={"text"} placeholder={"Enter search term"}/>
-        </label>
+        <div className={"flex flex-col"}>
+            <span className={"text-sm text-neutral-600 mb-2"}>Search</span>
+            <input className={"h-12 w-full p-4 rounded-lg bg-neutral-800 border-neutral-500 border text-sm"} type={"text"} placeholder={"Enter search term"}/>
+        </div>
     </>;
 };
 
