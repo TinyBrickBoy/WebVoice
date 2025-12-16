37a59640eb93a573bbb3f33054548cc6e0aa0f15
booky <booky@booky.dev>
2025-12-16 12:57:08 +0100

    Refactor DeviceSelectionDropdown component to separate file

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,89 +1,12 @@
 import type {FunctionComponent} from "preact";
 import Modal from "./Modal.tsx";
 import type {StateType} from "../scripts/types.ts";
-import {useCallback, useEffect, useState} from "preact/hooks";
-import {useVoiceStateContext} from "./VoiceStateProvider.tsx";
-import Dropdown from "./Dropdown.tsx";
 import VersionInfo from "./VersionInfo.tsx";
+import DeviceSelectionDropdown from "./DeviceSelectionDropdown.tsx";
 
 interface Props {
     visible: StateType<boolean>;
 }
-
-interface DropdownProps {
-    type: "input" | "output";
-}
-
-const DeviceSelectionDropdown: FunctionComponent<DropdownProps> = ({type}) => {
-    const {devices} = useVoiceStateContext();
-
-    const [deviceList, setDeviceList] = useState<MediaDeviceInfo[]>([]);
-    const refreshDeviceList = useCallback(() => {
-        const devicesPromise = devices[type === "input" ? "getMicrophoneList" : "getSpeakerList"]();
-        devicesPromise
-            .then(result => setDeviceList(result))
-            .catch(error => console.error(error));
-    }, [devices, type]);
-    useEffect(() => {
-        // initial refresh
-        refreshDeviceList();
-        // refresh when the device manager tells us to
-        return devices.getEvents().register(
-            "update_device_list",
-            () => refreshDeviceList(),
-        );
-    }, [devices, refreshDeviceList]);
-
-    const [deviceId, setDeviceId] = useState<string | null>(null);
-    const refreshDeviceId = useCallback(() => {
-        setDeviceId(devices[type === "input" ? "getMicrophoneId" : "getSpeakerId"]());
-    }, [devices, type]);
-    useEffect(() => {
-        // initial refresh
-        refreshDeviceId();
-        // refresh when the device manager tells us to
-        return devices.getEvents().register(
-            type === "input" ? "update_microphone" : "update_speaker",
-            () => refreshDeviceId(),
-        );
-    }, [devices, refreshDeviceId]);
-
-    const [updating, setUpdating] = useState<boolean>(false);
-    const updateDevice = useCallback((newDeviceId: string) => {
-        setUpdating(true);
-        devices[type === "input" ? "updateMicrophone" : "updateSpeaker"](newDeviceId)
-            .then(result => {
-                setUpdating(false);
-                if (result) {
-                    console.log(`Updated ${type} to ${newDeviceId}`);
-                } else {
-                    console.error(`Failed to update ${type} to ${newDeviceId}`);
-                }
-            })
-            .catch(error => {
-                setUpdating(false);
-                console.error(`Error on audio change for ${type} to ${newDeviceId}`, error);
-            });
-    }, [devices, type]);
-
-    if (deviceList.length < 1) {
-        return <>
-            <Dropdown disabled value={"empty"}>
-                <option value={"empty"} className={"italic"}>No devices found</option>
-            </Dropdown>
-        </>;
-    }
-    return <>
-        <Dropdown
-            value={deviceId || deviceList[0].deviceId} disabled={updating}
-            onUpdate={value => updateDevice(value)}
-        >
-            {deviceList.map(device => (
-                <option value={device.deviceId} key={device.deviceId}>{device.label}</option>
-            ))}
-        </Dropdown>
-    </>;
-};
 
 const SettingsModal: FunctionComponent<Props> = ({visible}) => {
 
