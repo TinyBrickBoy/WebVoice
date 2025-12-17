e23c491bb8ff2ad0d762c2ecce9dd0e77dec5e6b
SirReGa <jona.skrubel@gmail.com>
2025-12-17 23:37:26 +0100

    Redesign text input

--- src/components/common/TextBox.tsx
+++ src/components/common/TextBox.tsx
@@ -9,7 +9,7 @@ const TextBox: FunctionComponent<Props> = ({onChange, onSave, type, className, .
     return <>
         <input
             {...other} type={type || "text"}
-            className={`h-12 w-full p-4 rounded-xl text-sm bg-neutral-900 border border-neutral-700 placeholder:text-neutral-300 focus:outline-2 focus:outline-white disabled:cursor-not-allowed ${className || ""}`}
+            className={`h-12 w-full p-4 rounded-xl text-sm bg-neutral-900 border border-neutral-700 placeholder:text-neutral-300 focus:outline focus:outline-neutral-500 disabled:cursor-not-allowed ${className || ""}`}
             onInput={event => onChange(event.currentTarget.value)}
             onChange={event => !onSave || onSave(event.currentTarget.value)}
         />
