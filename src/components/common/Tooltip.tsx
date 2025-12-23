e1d7eac2f096c87c3e710e858d3476f550e44c75
booky <booky@booky.dev>
2025-12-23 02:26:34 +0100

    Fix tooltip z index if not hovering

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -12,7 +12,7 @@ const Tooltip: FunctionComponent<Props> = ({hint, align, children, className, ..
         <div {...other} className={`group/hinfo relative flex flex-col ${className || ""}`}>
             {hint && <div
                 aria-hidden={true}
-                className={`self-center opacity-0 group-hover/hinfo:opacity-100 transition-opacity duration-100 absolute bg-neutral-800 leading-none mb-1 mt-1 p-2 select-none rounded-lg ${align === "top" ? "bottom-full" : "top-full"} whitespace-nowrap z-10`}
+                className={`self-center opacity-0 group-hover/hinfo:opacity-100 group-hover/hinfo:z-10 transition-opacity duration-100 absolute bg-neutral-800 leading-none mb-1 mt-1 p-2 select-none rounded-lg ${align === "top" ? "bottom-full" : "top-full"} whitespace-nowrap`}
             >
                 {hint}
             </div>}
