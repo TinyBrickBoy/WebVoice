85513e122228c329f771c4551cdbbf596402d479
booky <booky@booky.dev>
2025-12-23 05:33:28 +0100

    Revert "Add goofy ah range input styling"
    This reverts commit 0375c6a8a01b6157ef2b3e89d3e922fad8859f29.

--- src/components/common/SliderInput.tsx
+++ src/components/common/SliderInput.tsx
@@ -10,45 +10,7 @@ const SliderInput: FunctionComponent<Props> = ({onChange, onSave, className, chi
         <label className={"flex flex-row gap-4 items-center"}>
             <input
                 {...other} type={"range"}
-                className={`
-                    grow cursor-pointer ${className || ""}
-                    h-6 hover:h-6 rounded-full
-                    duration-50
-                    
-                    [&::-webkit-slider-thumb]:duration-50
-                    [&::-webkit-slider-thumb]:h-4
-                    hover:[&::-webkit-slider-thumb]:h-4.5
-                    [&::-webkit-slider-thumb]:w-4
-                    hover:[&::-webkit-slider-thumb]:w-4.5
-                    [&::-webkit-slider-thumb]:border-0
-                    [&::-webkit-slider-thumb]:rounded-full
-                    [&::-webkit-slider-thumb]:cursor-pointer
-                    [&::-webkit-progress-value]:h-1
-                    hover:[&::-webkit-progress-value]:h-1.5
-                    [&::-webkit-progress-value]:rounded-full
-                    [&::-webkit-progress-value]:bg-indigo-500
-                    [&::-webkit-slider-runnable-track]:h-1
-                    hover:[&::-webkit-slider-runnable-track]:h-1.5
-                    [&::-webkit-slider-runnable-track]:bg-gray-500
-                    [&::-webkit-slider-runnable-track]:rounded-full
-                    
-                    [&::-moz-range-thumb]:duration-50
-                    [&::-moz-range-thumb]:h-4
-                    hover:[&::-moz-range-thumb]:h-4.5
-                    [&::-moz-range-thumb]:w-4
-                    hover:[&::-moz-range-thumb]:w-4.5
-                    [&::-moz-range-thumb]:border-0
-                    [&::-moz-range-thumb]:rounded-full
-                    [&::-moz-range-thumb]:cursor-pointer
-                    [&::-moz-range-progress]:h-1
-                    hover:[&::-moz-range-progress]:h-1.5
-                    [&::-moz-range-progress]:rounded-full
-                    [&::-moz-range-progress]:bg-indigo-500
-                    [&::-moz-range-track]:h-1
-                    hover:[&::-moz-range-track]:h-1.5
-                    [&::-moz-range-track]:bg-gray-500
-                    [&::-moz-range-track]:rounded-full
-                `}
+                className={`grow rounded-lg bg-neutral-500 accent-indigo-500 cursor-pointer ${className || ""}`}
                 onInput={({currentTarget: {value}}) => onChange(Number(value))}
                 onChange={({currentTarget: {value}}) => !onSave || onSave(Number(value))}
             />
